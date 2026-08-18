52f10d13c998cd2de5d9802691d14cd59366ac87
lucastranchitella <lucastranchi@gmail.com>
2026-08-18 20:02:30 -0300

    integração com css e java

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('meuBotao').addEventListener('click', function() {
+    alert('Olá! O JavaScript está funcionando perfeitamente.');
+    document.getElementById('titulo').style.color = '#e74c3c';
+});
